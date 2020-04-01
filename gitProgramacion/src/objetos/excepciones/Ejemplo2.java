f4ef64b47e45b3cc94410159b25f650b2601564e
javie <javie@LAPTOP-25FADA29>
2020-04-01 14:54:09 +0200

    clase 01-04

--- gitProgramacion/src/objetos/excepciones/Ejemplo2.java
+++ gitProgramacion/src/objetos/excepciones/Ejemplo2.java
@@ -10,10 +10,13 @@ public class Ejemplo2 {
 		 * Programa que pide al usuario un numero que tiene que estar entre el 10 y el 20
 		 */
 		Scanner sc= new Scanner(System.in);
+		//La variable correcto ba a controlar si se repite la peticion de datos
 		boolean correcto=false;
 		while(!correcto) {
 			System.out.println("Introduce un numero entre 10 y 20: ");
+			//Dentro del try va lo que es susceptible de fallar
 			try {
+				//La siguiente linea "peta" si el usuario no mete un numero
 				int numero=sc.nextInt();
 				if(numero>=10 && numero<=20) {
 					correcto=true;
@@ -22,8 +25,10 @@ public class Ejemplo2 {
 				}
 			}catch(InputMismatchException nombreExcepcion) {
 				System.out.println("Numero mal introducido");
+				//Reinicio el escanner porque habra fallado
 				sc=new Scanner(System.in);
-				
+			}catch(Exception e) {
+				System.out.println("Se ha producido un error");
 			}
 		}
 	}
