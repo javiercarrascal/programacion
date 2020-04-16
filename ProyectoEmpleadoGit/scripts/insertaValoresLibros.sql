ab5704a7bb4b249136919148cafb1975311c342f
javie <javie@LAPTOP-25FADA29>
2020-04-16 11:56:51 +0200

    autores

--- ProyectoEmpleadoGit/scripts/insertaValoresLibros.sql
+++ ProyectoEmpleadoGit/scripts/insertaValoresLibros.sql
@@ -9,7 +9,6 @@ INSERT  INTO  Libros   VALUES  ( 2 ,  '84-205-3701-2' , 'Java 2 .   Volumen 2 .'
 INSERT INTO Libros VALUES ( 3 , '0-131-00287-2' , 'Thinking in Java (3rd Edition )' , 'Prentice Hall PTR' , '2002-01-01');
 INSERT INTO Libros VALUES ( 4 , '0-130-35313-2' , 'Thinking in C++, Vol.2 :practical Programming , Second Edition' , 'prentince Hall' , '2003-01-01' ) ;
 
-INSERT INTO autor_libro VALUES ( 1 , 2 ) ;
 INSERT INTO autor_libro VALUES ( 2 , 2 ) ; 
 INSERT INTO autor_libro VALUES ( 3 , 1 ) ; 
 INSERT INTO autor_libro VALUES ( 4 , 1 ) ; 
