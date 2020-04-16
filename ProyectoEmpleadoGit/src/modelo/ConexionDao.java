ed5d33ca068ac23c7ae9ee4f069c35360761d99d
javie <javie@LAPTOP-25FADA29>
2020-04-16 11:54:19 +0200

    conexionDAO

--- ProyectoEmpleadoGit/src/modelo/ConexionDao.java
+++ ProyectoEmpleadoGit/src/modelo/ConexionDao.java
@@ -2,11 +2,15 @@ package modelo;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+//DAO data access object
 public class ConexionDao {
 	public static Connection conexion;
+	
+	//Constructor
 	public ConexionDao(){
-		this.conexion=conexion();
+		ConexionDao.conexion=conexion();
 	}
+	
 	public static Connection getConexion() {
 		return conexion;
 	}
@@ -46,11 +50,20 @@ public class ConexionDao {
 		try {
 			//Busca la clase correcpondiente en las librerias importadas
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection con=DriverManager.getConnection("jdbc:mysql://localhost/bd_empleado?serverTimezone=UTC","root","root");
+			//Localhost : La base de datos es local, sino pondriamos una ip
+			//3306: es el puerto de mysql
+			//proyecto_empleados : nombre del proyecto
+			//serverTimezone=UTC : uso horario de la bbdd
+			//1: usuario
+			//2: contraseña
+			Connection con=DriverManager.getConnection
+					("jdbc:mysql://localhost:3306/proyecto_empleados?serverTimezone=UTC","root","1234");
 			System.out.println("Conexion con base de datos realizada");
 			return con;
+			
 		} catch (ClassNotFoundException e) {
-			System.err.println("Clase mysql no encontrado");
+			//Entra a este catch si no se encuentra la clase
+			System.err.println("Clase mysql no encontrada");
 		} catch (SQLException e) {
 			System.err.println("Error en la conexion");
 		}
---
+++ ProyectoEmpleadoGit/src/vista/Pruebas.java
@@ -0,0 +1,12 @@
+package vista;
+
+import modelo.ConexionDao;
+
+public class Pruebas {
+
+	public static void main(String[] args) {
+		ConexionDao cd = new ConexionDao();
+
+	}
+
+}
